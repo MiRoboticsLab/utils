6648f5a7476526af2060f83845e95973f64fae79
dukun <dukun1@xiaomi.com>
2022-02-08 17:11:51 +0800

    [modify] update readme
    descriptions:
    1. modify some cyberdog_json api.
    2. add API Comments for cyberdog_json and cyberdog_toml.
    3. update README.md.
    
    Signed-off-by: dukun <dukun1@xiaomi.com>

--- cyberdog_common/README.md
+++ cyberdog_common/README.md
@@ -1,8 +1,112 @@
-对一些通用第三块的封装
+# Cyberdog基础功能API封装
 
-具体包括：
-1. CyberdogJson: json的封装库，目前采用rapidjson；
-该类中API均为static类型，标注类名直接调用即可；
-如源码中几处public标注所示，主要分为add、get和serialize三个类型的API；
-具体使用方法可以参考test中的json_test.cpp；
+**具体包括：**
+
+- json
+- toml
+
+
+
+## Json
+
+1. 采用 [rapidjson](https://github.com/Tencent/rapidjson) , V1.1.0
+
+2.  代码中使用json，只需引入头文件即可，封装的API位于该头文件中：
+
+   cmakelsit.txt:
+
+   ```cmake
+   find_package(rapidjson REQUIRED)
+   
+   ament_target_dependencies(${your_executable} rapidjson)
+   ```
+
+   package.xml:
+
+   ```xml
+   <depend>rapidjson</depend>
+   ```
+
+   source code:
+
+   ```c++
+   #include "cyberdog_common/cyberdog_json.hpp"
+   
+   # 另外，全部功能位于命名空间cyberdog::common之内.
+   ```
+
+3. 该封装只做了序列化场景的封装，并非全部API，这点要注意。主要包括Add(修改值), Get(获取值), ToString（序列化）, FromString(反序列化), ToFile(存储), FromFile(读取).
+
+4. 若要进一步或直接使用rapidjson, 其源码位于"cyberdog_ws/third_party/rapidjson"，但不推荐这么做。如果发生第三方库的更换，其回归工作量可能较大。
+
+5. 为了做基本的隔离，请使用"json"代替"rapidjson"命名空间
+
+   ```c++
+   # cyberdog_json.hpp
+   namespace cyberdog
+   {
+   namespace common
+   {
+   /* 要求使用json代替rapidjson命名空间 */
+   namespace json = rapidjson;
+   }  // namespace common
+   }  // namespace cyberdog
+   
+   # 使用时
+   cyberdog::common::json Document d(cyberdog::common::json::kObjectType);
+   # 当然你可以使用自己的别名，例如：
+   namespace json = cyberdog::common::json;
+   ```
+
+6. 基本应用请参考json_test.cpp.
+
+7. 更多参考：
+
+   [rapidjson教程](http://rapidjson.org/zh-cn/index.html)
+
+   [json协议规范](https://www.rfc-editor.org/info/rfc7159)
+
+
+
+## Toml
+
+1. 采用 [toml11](https://github.com/ToruNiina/toml11) , V3.7.0;
+
+2. 引入方式同json：
+
+3.  代码中使用json，只需引入头文件即可，封装的API位于该头文件中：
+
+   cmakelsit.txt:
+
+   ```cmake
+   find_package(toml REQUIRED)
+   
+   ament_target_dependencies(${your_executable} toml)
+   ```
+
+   package.xml:
+
+   ```xml
+   <depend>toml</depend>
+   ```
+
+   source code:
+
+   ```c++
+   #include "cyberdog_common/cyberdog_toml.hpp"
+   
+   # 另外，全部功能位于命名空间cyberdog::common之内.
+   ```
+
+4. 若要进一步或直接使用toml11, 其源码位于"cyberdog_ws/third_party/toml"，但不推荐这么做。如果发生第三方库的更换，其回归工作量可能较大。
+
+5. 基本应用请参考json_test.cpp.
+
+6. 更多参考：
+
+   [toml11](https://github.com/ToruNiina/toml11)
+
+   [toml协议](https://github.com/toml-lang/toml)
+
+   [toml配置入门](https://mojotv.cn/2018/12/26/what-is-toml)
 
--- cyberdog_common/test/toml_test.cpp
+++ cyberdog_common/test/toml_test.cpp
@@ -1,3 +1,16 @@
+// Copyright (c) 2021 Beijing Xiaomi Mobile Software Co., Ltd. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 /**
  * @file toml_test.cpp
  * @author dukun (dukun1@xiaomi.com)
@@ -7,8 +20,6 @@
  * @version 0.1
  * @date 2022-02-05
  *
- * @copyright Copyright (c) 2022. MiRobotlab Rop Team.
- *
  */
 #include <dirent.h>
 #include <stdio.h>
@@ -17,7 +28,6 @@
 #include "gtest/gtest.h"
 #include "cyberdog_common/cyberdog_toml.hpp"
 using namespace cyberdog::common;
-// using namespace toml;
 
 TEST(hello, hello__Test)
 {
@@ -29,10 +39,10 @@ TEST(file, parser)
 {
   toml::value value;
   auto result = CyberdogToml::ParseFile("nofile", value);
-  ASSERT_FALSE(result);
+  EXPECT_FALSE(result);
 
   result = CyberdogToml::ParseFile(std::string(BenchmarkPath) + "/benchmark.toml", value);
-  ASSERT_TRUE(result);
+  EXPECT_TRUE(result);
 }
 
 TEST(reader, table)
@@ -44,28 +54,28 @@ TEST(reader, table)
 
   toml::value a;
   auto result = CyberdogToml::Get(value, "a", a);
-  ASSERT_TRUE(result);
-  ASSERT_TRUE(a.is_table());
+  EXPECT_TRUE(result);
+  EXPECT_TRUE(a.is_table());
 
   int aa;
   result = CyberdogToml::Get(a, "a", aa);
-  ASSERT_TRUE(result);
-  ASSERT_EQ(aa, 1);
+  EXPECT_TRUE(result);
+  EXPECT_EQ(aa, 1);
 
   std::string ab;
   result = CyberdogToml::Get(a, "b", ab);
-  ASSERT_TRUE(result);
-  ASSERT_EQ(ab, std::string("cyberdog"));
+  EXPECT_TRUE(result);
+  EXPECT_EQ(ab, std::string("cyberdog"));
 
   bool ac;
   result = CyberdogToml::Get(a, "c", ac);
-  ASSERT_TRUE(result);
-  ASSERT_EQ(ac, true);
+  EXPECT_TRUE(result);
+  EXPECT_EQ(ac, true);
 
   float ad;
   result = CyberdogToml::Get(a, "d", ad);
-  ASSERT_TRUE(result);
-  ASSERT_EQ(ad, static_cast<float>(0.1));
+  EXPECT_TRUE(result);
+  EXPECT_EQ(ad, static_cast<float>(0.1));
 }
 
 TEST(reader, array)
@@ -77,16 +87,16 @@ TEST(reader, array)
 
   toml::value b;
   auto result = CyberdogToml::Get(value, "b", b);
-  ASSERT_TRUE(result);
-  ASSERT_TRUE(b.is_array());
-  ASSERT_TRUE(b.as_array().at(0).is_table());
+  EXPECT_TRUE(result);
+  EXPECT_TRUE(b.is_array());
+  EXPECT_TRUE(b.as_array().at(0).is_table());
 
   toml::value b0;
   result = CyberdogToml::Get(b, 0, b0);
-  ASSERT_TRUE(result);
-  ASSERT_TRUE(b0.is_table());
+  EXPECT_TRUE(result);
+  EXPECT_TRUE(b0.is_table());
   result = CyberdogToml::Get(b, 10, b0);
-  ASSERT_FALSE(result);
+  EXPECT_FALSE(result);
 }
 
 TEST(writer, modify)
@@ -98,18 +108,18 @@ TEST(writer, modify)
 
   toml::value b;
   auto result = CyberdogToml::Get(value, "b", b);
-  ASSERT_TRUE(result);
+  EXPECT_TRUE(result);
   result = CyberdogToml::Set(b, "a", 0);
-  ASSERT_FALSE(result);
+  EXPECT_FALSE(result);
 
   toml::value b1;
   result = CyberdogToml::Get(b, 1, b1);
-  ASSERT_TRUE(result);
+  EXPECT_TRUE(result);
   result = CyberdogToml::Set(b1, "c", true);
-  ASSERT_TRUE(result);
+  EXPECT_TRUE(result);
   bool b1c;
   result = CyberdogToml::Get(b1, "c", b1c);
-  ASSERT_TRUE(b1c);
+  EXPECT_TRUE(b1c);
 }
 
 TEST(writer, add)
@@ -118,25 +128,25 @@ TEST(writer, add)
   std::cout << a.is_uninitialized() << std::endl;
   std::cout << a.is_table() << std::endl;
   auto result = CyberdogToml::Set(a, "b", std::string("xiaomi"));
-  ASSERT_TRUE(result);
+  EXPECT_TRUE(result);
 
   std::string ab;
   result = CyberdogToml::Get(a, "b", ab);
-  ASSERT_TRUE(result);
-  ASSERT_EQ(ab, std::string("xiaomi"));
+  EXPECT_TRUE(result);
+  EXPECT_EQ(ab, std::string("xiaomi"));
 
   toml::value b;
   result = CyberdogToml::Set(b, 0, std::string("xiaomi"));
-  ASSERT_FALSE(result);
+  EXPECT_FALSE(result);
 
   result = CyberdogToml::Set(b, std::string("xiaomi"));
-  ASSERT_TRUE(result);
-  ASSERT_TRUE(b.is_array());
+  EXPECT_TRUE(result);
+  EXPECT_TRUE(b.is_array());
 
   std::string b0;
   result = CyberdogToml::Get(b, 0, b0);
-  ASSERT_TRUE(result);
-  ASSERT_EQ(b0, std::string("xiaomi"));
+  EXPECT_TRUE(result);
+  EXPECT_EQ(b0, std::string("xiaomi"));
 }
 
 int main(int argc, char ** argv)
